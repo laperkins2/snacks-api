d647c54b8f27a7cfcf66947bff51b4f23dcff764
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2024-09-09 21:00:26 -0500

    configured for vercel

--- supaInstance.js
+++ supaInstance.js
@@ -5,10 +5,11 @@ const axios = require('axios');
 
 const instance = axios.create({
   baseURL: process.env.SUPABASE_URL + '/rest/v1',
-  timeout: 1000,
+  timeout: 10000,
   headers: {
     apikey: process.env.SUPABASE_KEY,
     Authorization: 'Bearer ' + process.env.SUPABASE_KEY,
+    'Content-Type': 'application/json',
   },
 });
 
