b6deee0010a468f262355714fe2d9c0f5a160b8a
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2024-09-11 10:37:04 -0500

    took out snacks array

--- api/index.js
+++ api/index.js
@@ -19,92 +19,14 @@ const app = express();
 //define a port
 const PORT = 4000;
 
-const SNACKS = [
-  {
-    id: 1,
-    name: 'Chips',
-    description: 'Crunchy and salty potato chips.',
-    price: 2.99,
-    category: 'Salty Snacks',
-    inStock: true,
-  },
-  {
-    id: 2,
-    name: 'Chocolate Bar',
-    description: 'Rich and creamy milk chocolate bar.',
-    price: 1.49,
-    category: 'Sweet Snacks',
-    inStock: true,
-  },
-  {
-    id: 3,
-    name: 'Popcorn',
-    description: 'Buttery and fluffy popcorn.',
-    price: 3.49,
-    category: 'Salty Snacks',
-    inStock: false,
-  },
-  {
-    id: 4,
-    name: 'Gummy Bears',
-    description: 'Colorful and chewy gummy bears.',
-    price: 2.19,
-    category: 'Sweet Snacks',
-    inStock: true,
-  },
-  {
-    id: 5,
-    name: 'Pretzels',
-    description: 'Crispy and twisted pretzels.',
-    price: 2.79,
-    category: 'Salty Snacks',
-    inStock: true,
-  },
-  {
-    id: 6,
-    name: 'Granola Bar',
-    description: 'Healthy and crunchy granola bar.',
-    price: 1.99,
-    category: 'Healthy Snacks',
-    inStock: true,
-  },
-  {
-    id: 7,
-    name: 'Fruit Snacks',
-    description: 'Sweet and fruity gummy snacks.',
-    price: 2.49,
-    category: 'Sweet Snacks',
-    inStock: false,
-  },
-  {
-    id: 8,
-    name: 'Nuts Mix',
-    description: 'A mix of roasted and salted nuts.',
-    price: 4.99,
-    category: 'Healthy Snacks',
-    inStock: true,
-  },
-  {
-    id: 9,
-    name: 'Energy Bar',
-    description: 'High-protein energy bar.',
-    price: 2.59,
-    category: 'Healthy Snacks',
-    inStock: true,
-  },
-  {
-    id: 10,
-    name: 'Rice Crackers',
-    description: 'Light and crispy rice crackers.',
-    price: 3.19,
-    category: 'Healthy Snacks',
-    inStock: false,
-  },
-];
-
 // define Middleware functions defined before routes
 //use cors middleware
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:4000',
+  optionsSuccessStatus: 200, //some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 //use JSON middleware to parse request bodies
 app.use(express.json());
@@ -116,7 +38,7 @@ app.get('/', (req, res, next) => {
 });
 
 //Route to Get all Snacks
-app.get('/snacks', async (req, res, next) => {
+app.get('/snacks', cors(corsOptions), async (req, res, next) => {
   try {
     // res.json(SNACKS);
     const response = await supabase.get('/snacks');
