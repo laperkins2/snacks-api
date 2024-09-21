978737329e03cefb6a35370c8536a38396d9803e
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2024-09-21 12:21:03 -0500

    added caching for performance

--- api/routes/getAll.js
+++ api/routes/getAll.js
@@ -1,8 +1,19 @@
 const supabase = require('../../supaInstance');
 
+const cache = {};
+
 const getAll = async (req, res, next) => {
   try {
+    //check if we have the data in our cache
+    if (cache.snacks) {
+      console.log('Using cache');
+      return res.json(cache.snacks);
+    }
+
     const response = await supabase.get('/snacks');
+    //add the response data to our cache
+    cache.snacks = response.data;
+    console.log('Adding to cache');
     res.json(response.data);
   } catch (error) {
     next(error);
